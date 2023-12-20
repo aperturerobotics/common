7dd3a8ef6212631426071e5bf8794ca4a67efbff
Christian Stewart <christian@aperture.us>
2023-12-20 15:42:10 -0800

    feat: add react hooks eslint plugin
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
     'prettier',
   ],
   parserOptions: {
