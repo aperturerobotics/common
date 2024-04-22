ca8a680eb248f9dbfcfaf68bf843007beed79b8b
Christian Stewart <christian@aperture.us>
2024-04-21 21:08:34 -0700

    refactor: switch to protoc-gen-es-starpc
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- hack/tools.go
+++ hack/tools.go
@@ -12,14 +12,16 @@ import (
 	_ "github.com/aperturerobotics/goprotowrap/cmd/protowrap"
 	// _ imports protoc-gen-go-lite
 	_ "github.com/aperturerobotics/protobuf-go-lite/cmd/protoc-gen-go-lite"
+	// _ imports protoc-gen-starpc
+	_ "github.com/aperturerobotics/starpc/cmd/protoc-gen-go-starpc"
+
 	// _ imports golangci-lint
 	_ "github.com/golangci/golangci-lint/pkg/golinters"
 	// _ imports golangci-lint commands
 	_ "github.com/golangci/golangci-lint/pkg/commands"
+
 	// _ imports go-mod-outdated
 	_ "github.com/psampaz/go-mod-outdated"
-	// _ imports protoc-gen-starpc
-	_ "github.com/aperturerobotics/starpc/cmd/protoc-gen-go-starpc"
 	// _ imports goimports
 	_ "golang.org/x/tools/cmd/goimports"
 	// _ imports gofumpt
