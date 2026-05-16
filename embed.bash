9fb3a17a7595c815a6916a2e649692f89d05afdd
Christian Stewart <christian@aperture.us>
2026-05-16 12:35:12 -0700

    fix(build): make tool embedding portable
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- embed.bash
+++ embed.bash
@@ -5,4 +5,4 @@ set -x
 cp ./tools/go.mod ./go.mod.tools
 cp ./tools/go.sum ./go.sum.tools
 cp ./tools/deps.go ./deps.go.tools
-sed -i '/github.com\/aperturerobotics\/common/d' go.mod.tools go.sum.tools deps.go.tools
+perl -0pi -e 's/^.*github\.com\/aperturerobotics\/common.*\n//mg' go.mod.tools go.sum.tools deps.go.tools
